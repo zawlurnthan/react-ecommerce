0fce4295f6f679739bf37a3c623f3341c2995959
Zaw L Than <zawlurnthan@gmail.com>
2022-01-11 15:47:27 -0700

    updating shop page, collection preview component and items.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import './App.css';
 
 
@@ -10,6 +11,7 @@ function App() {
     <div>
         <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/shop" element={<ShopPage />} />
         </Routes>
     </div>
   );
