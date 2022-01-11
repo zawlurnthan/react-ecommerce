513b17ddbfb0cb47c7041a46949a2ee1e3a08f8f
Zaw L Than <zawlurnthan@gmail.com>
2022-01-11 14:00:31 -0700

    update menu-items to navigate with useNavigate.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import './App.css';
+
+
 
 function App() {
   return (
     <div>
-        <Homepage />
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  // </React.StrictMode>,
+    <BrowserRouter>
+           <App />
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
